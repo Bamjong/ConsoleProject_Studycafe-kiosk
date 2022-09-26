400ae25a9e53bee7ca78f429497af3693bff07f4
Bamjong <qjawhd100@naver.com>
2022-09-26 20:07:40 +0900

    최종

--- src/com/victoree2/common/AccountData.java
+++ src/com/victoree2/common/AccountData.java
@@ -10,47 +10,33 @@ public class AccountData implements Serializable{
 	private String name; //이름
 	private String password; //패스워드
 	private String birthday; //생일
-	private String phoneNumber;
-	private int status; //계정 상태 (1 : 사용자  0 : 계정 정지  9 : 관리자 2: 구매한사람 3:환불한사람)
-	private boolean checkIn;// 입실/퇴실
-	private int cnt;//경고횟수
-	private int price;//결제금액
-	
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getPrice() {
-		return price;
-	}
+	private String phonenumber;
+	private String startday; //결제일
+	private String endday; //종료일
+	private int status; //계정 상태 (1 : 사용자  0 : 계정 정지  9 : 관리자)
+	//private boolean  //입실/퇴실
+		//정기권
 	private static final long serialVersionUID = (long)5746694569156904189.0;
 	//serialversionuid 값을 지정을 안하면 로컬과 클라이언트에서 자동으로 생성되는 hash값으로 에러 발생함.
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public AccountData(String id, String name, String password, String birthday, String phoneNumber) {
+	
+
+	public AccountData(String id, String name, String password, String birthday, String phonenumber, String startday, String endday) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 		this.birthday = birthday;
-		this.phoneNumber = phoneNumber;
+		this.phonenumber = phonenumber;
+		this.startday = startday;
+		this.endday = endday;
 		this.status = 1;
-		this.checkIn = false;
-		this.price = 0;
 	}
-	public boolean getCheckIn() {
-		return checkIn;
-	}
-	public void setCheckIn(boolean checkIn) {
-		this.checkIn = checkIn;
-	}
-	public AccountData(String id, String name, String password, String birthday, int status) {
+	public AccountData(String id, String name, String password, String birthday, String startday, String endday, int status) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 		this.birthday = birthday;
+		this.startday = startday;
+		this.endday = endday;
 		this.status = status;
 	}
 	public String getId() {
@@ -69,23 +55,28 @@ public class AccountData implements Serializable{
 	public String getBirthday() {
 		return birthday;
 	}
+	public String getStartday() {
+		return startday;
+	}
+	public void setStartday(String startday) {
+		this.startday = startday;
+	}
+	public String getEndday() {
+		return endday;
+	}
+	public void setEndday(String endday) {
+		this.endday = endday;
+	}
 	public int getStatus() {
 		return status;
 	}
 	public void setStatus(int status) {
 		this.status = status;
 	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt() {
-		cnt++;
-	}
 	@Override
 	public String toString() {
-		return "[ID:"+id+" 이름:" + name + "]";
-//		return "Account [id=" + id + ", name=" + name + ", password=" + password + ", birthday=" + birthday
-//				+ ", status=" + status + "]";
+		return "Account [id=" + id + ", name=" + name + ", password=" + password + ", birthday=" + birthday
+				+ ", startday=" + startday + ", endday=" + endday + ", status=" + status + "]";
 	}
 	
 	
--- src/com/victoree2/system/AccountSystem.java
+++ src/com/victoree2/system/AccountSystem.java
@@ -14,13 +14,13 @@ import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import com.victoree2.common.AccountData;
-import com.victoree2.common.ReturnMessage;
+import com.victoree2.common.AccountInterface;
+import com.victoree2.common.returnMessage;
 import com.victoree2.main.ReadingRoom;
 
-public class AccountSystem extends ReturnMessage{
+public class AccountSystem extends returnMessage implements AccountInterface{
 	
 	Scanner scan = new Scanner(System.in);
 	ReadingRoom room = new ReadingRoom();
@@ -28,14 +28,9 @@ public class AccountSystem extends ReturnMessage{
 	//key 값으로 id 값을 줄것이며 id값은 중복이 불가능하다.
 	private HashMap<String, AccountData> account = new HashMap<String, AccountData>();
 	
-	boolean adminCheck = true;
+	boolean admin_check = true;
 	private String id;
-	private String name;
 	private String password;
-	private String birthday;
-	private String phonenumber;
-	
-	
 
 	
 	//파일 직렬화
@@ -51,7 +46,7 @@ public class AccountSystem extends ReturnMessage{
 	
 	Set<String> set;
 	
-	String filename = "C:\\KOSA_IT\\login\\UserDB.txt";	
+	String filename = "C:\\KOSA_IT\\login\\UserDB1.txt";	
 	File file = new File(filename);
 	
 	
@@ -59,93 +54,44 @@ public class AccountSystem extends ReturnMessage{
 		return account;
 	}
 
-	public void signUP(){ //회원가입
-		String idReg = "^[A-za-z0-9]{5,15}";  // [영문 대문자 또는 소문자 또는 숫자로 시작하는 아이디, 길이는 5~15자, 끝날때 제한 없음]
-		String pwReg = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"; // [최소 8 자, 최소 하나의 문자 및 하나의 숫자]
-		String birthReg = "^([0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[1,2][0-9]|3[0,1]))"; // [970219 이런 형식으로]
-		String phoneReg = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$"; // [하이픈 제외]
-		
-
-		
+	@Override
+	public void signUP(){
+		String regex = "^[a-zA-Z]{1}[a-zA-Z0-9_]{4,11}";
 		boolean regexCheck;
-		if(!adminCheck) {
+		if(!admin_check) {
 			System.out.println(message(room.language, "0007")+" "+message(room.language, "0005")+message(room.language, "0006"));
 			System.out.println(message(room.language, "0008")+" " + message(room.language, "0009") + " : ");
 			String id = scan.nextLine();
-			
 			System.out.println(message(room.language, "0010") + " " + message(room.language, "0009") + " : ");
 			String password = scan.nextLine();
-			AccountData ac = new AccountData(id, "관리자", password, "00000000", 9);
+			AccountData ac = new AccountData(id, "관리자", password, "00000000", "00000000", "99999999", 9);
 			account.put(id, ac); 
 			
 			System.out.println(message(room.language, "0007")+" " +message(room.language, "0011"));
-			adminCheck = true;
+			admin_check = true;
 		}
 		else {
 			System.out.println(message(room.language, "0022"));
-			
-			
-			while(true) {
-				System.out.println(message(room.language, "0008")+" " + message(room.language, "0009") + " : [길이 5~15자]");
-				this.id = scan.nextLine();
-				if(account.containsKey(id)) {
-					System.out.println("이미 존재하는 아이디입니다.");
-					return;
-				}
-				if(Pattern.matches(idReg,this.id)) {
-					break;
-				} else {            
-				    System.out.println("올바른 아이디 형식이 아닙니다. ");
-				}
-			}
-			
+			System.out.println(message(room.language, "0008")+" " + message(room.language, "0009") + " : ");
+			String id = scan.nextLine();
 			System.out.println(message(room.language, "0023") + " " + message(room.language, "0009") + " : ");
-			this.name = scan.nextLine();
-			
-			while(true) {
-				System.out.println(message(room.language, "0010") + " " + message(room.language, "0009") + " : [최소 8자 + 문자 숫자조합]");
-				this.password = scan.nextLine();
-				if(Pattern.matches(pwReg,this.password)) {
-					break;
-				} else {            
-				    System.out.println("올바른 비밀번호 형식이 아닙니다. ");
-				}
-			}
-			
-			while(true) {
-				System.out.println(message(room.language, "0024") + " " + message(room.language, "0009") + " : [970219 이런 형식으로]");
-				this.birthday = scan.nextLine();
-				if(Pattern.matches(birthReg,this.birthday)) {
-					break;
-				} else {            
-				    System.out.println("올바른 생일 형식이 아닙니다. ");
-				}
-			}
-			
-			while(true) {
-				System.out.println(message(room.language, "0025") + " " + message(room.language, "0009") + " : [하이픈 제외]");
-				this.phonenumber = scan.nextLine();
-				if(Pattern.matches(phoneReg,this.phonenumber)) {
-					break;
-				} else {            
-				    System.out.println("올바른 번호 형식이 아닙니다. ");
-				}
-			}
-			
-			AccountData ac = new AccountData(id, name, password, birthday, phonenumber);
+			String name = scan.nextLine();
+			System.out.println(message(room.language, "0010") + " " + message(room.language, "0009") + " : ");
+			String password = scan.nextLine();
+			System.out.println(message(room.language, "0024") + " " + message(room.language, "0009") + " : ");
+			String birthday = scan.nextLine();
+			System.out.println(message(room.language, "0025") + " " + message(room.language, "0009") + " : ");
+			String phonenumber = scan.nextLine();
+			AccountData ac = new AccountData(id, name, password, birthday,phonenumber, "00000000", "99999999");
 			account.put(id, ac);
 		}
 		System.out.println(message(room.language, "0011"));
 		save();
 	}
-	//업데이트
-	public void update(AccountData ac){
-		load();// 수정.
-		this.account.remove(ac.getId());
-		account.put(ac.getId(), ac);
-		save();
-	}
-	public AccountData login() { //로그인
+
+
+	@Override
+	public AccountData login() {
 		System.out.println(message(room.language, "0012"));
 		System.out.println(message(room.language, "0008")+" " + message(room.language, "0009") + " : ");
 		String id = scan.nextLine();
@@ -166,15 +112,17 @@ public class AccountSystem extends ReturnMessage{
 		return returnValue;
 	}
 
-	public void delete(HashMap<String, AccountData> account) {//계정삭제
-		this.account = account;
+	@Override
+	public void delete(HashMap<String, AccountData> account) {
+		this.account = account;	
 	}
 
-	public void resetPassword() {//패스워드초기화
+	@Override
+	public void resetPassword() {
 		// TODO Auto-generated method stub
 		
 	}
-	
+	@Override
 	public void save(){
 		try {
 			fos = new FileOutputStream(file);
@@ -182,7 +130,6 @@ public class AccountSystem extends ReturnMessage{
 			out = new ObjectOutputStream(bos);
 			
 			out.writeObject(account);
-//			System.out.println("save success");/////
 		} catch (Exception e) {
 			System.out.println(e);
 		}finally {
@@ -195,7 +142,7 @@ public class AccountSystem extends ReturnMessage{
 			}
 		}
 	}
-	
+	@Override
 	public void load() {
 		//역질렬화로 계정을 정보를 읽음.
 
@@ -203,10 +150,11 @@ public class AccountSystem extends ReturnMessage{
 			// 파일이 없을경우 생성.
 			if (!file.exists())
 				file.createNewFile();
+
 				fis = new FileInputStream(file);
 //				if (fis.read() == -1) {
 				if (fis.getChannel().size() == 0) {
-					adminCheck = false;
+					admin_check = false;
 					signUP();
 					// 관리자 계정이 없을경우 생성을 해주며 아래 fis를 계정이 있는 상태의 파일로 읽으므로서 예외발생 x
 					fis = new FileInputStream(file);
@@ -225,13 +173,13 @@ public class AccountSystem extends ReturnMessage{
 			
 		} catch (FileNotFoundException e) {
 			//User 클래스 생성자때 파일 자체를 만들지만 폴더 접근 권한이나 다른 이상현상으로 파일 생성이 안될경우 메세지 출력
-		//	System.out.println(message(room.language, "0014"));
+			System.out.println(message(room.language, "0014"));
 		} catch (EOFException e) {
-		//	System.out.println(message(room.language, "0015") + e.getMessage());
+			System.out.println(message(room.language, "0015") + e.getMessage());
 		} catch (IOException e) {
-		//	System.out.println(message(room.language, "0016")+ e.getMessage());	
+			System.out.println(message(room.language, "0016")+ e.getMessage());	
 		}  catch (Exception e) {
-		//	System.out.println(message(room.language, "0017") + e.getMessage());
+			System.out.println(message(room.language, "0017") + e.getMessage());
 		}finally {
 			try {
 				in.close();
